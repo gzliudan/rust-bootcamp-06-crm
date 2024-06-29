4c6814347ce4ea284db186e91146a808bf995c3a
Daniel Liu <liudaniel@qq.com>
2024-06-29 18:27:52 +0800

    feature: set output directory of prost_build

--- crm/build.rs
+++ crm/build.rs
@@ -1,6 +1,12 @@
 use anyhow::Result;
+use prost_build::Config;
+use std::fs;
 
 fn main() -> Result<()> {
-    prost_build::compile_protos(&["../protos/crm.proto"], &["../protos"])?;
+    fs::create_dir_all("src/pb")?;
+    let mut config = Config::new();
+    config
+        .out_dir("src/pb")
+        .compile_protos(&["../protos/crm.proto"], &["../protos"])?;
     Ok(())
 }
--- crm/src/main.rs
+++ crm/src/main.rs
@@ -1,30 +1,6 @@
-use crate::pb::*;
 use anyhow::Result;
+use crm::pb::User;
 use prost::Message;
-
-pub mod pb {
-    use prost_types::Timestamp;
-    use std::time::SystemTime;
-
-    include!(concat!(env!("OUT_DIR"), "/crm.rs"));
-
-    impl User {
-        pub fn new(id: u64, name: &str, email: &str) -> Self {
-            let now = SystemTime::now()
-                .duration_since(SystemTime::UNIX_EPOCH)
-                .unwrap();
-            Self {
-                id,
-                name: name.to_string(),
-                email: email.to_string(),
-                created_at: Some(Timestamp {
-                    seconds: now.as_secs() as i64,
-                    nanos: now.subsec_nanos() as i32,
-                }),
-            }
-        }
-    }
-}
 
 fn main() -> Result<()> {
     let user = User::new(1, "John Doe", "john.doe@example.com");
